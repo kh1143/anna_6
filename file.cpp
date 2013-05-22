16579ddded9487fc2c5c9ee3764963e497bc6d9b
kh1143 <kh-1143@hanmail.net>
2013-05-22 19:39:57 +0900

    file I/O

--- file.cpp
+++ file.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+<<<<<<< HEAD
 using namespace std;
 
 void readFile(char filename[])
@@ -18,13 +19,50 @@ void readFile(char filename[])
 //	}
 	
 	r.close();
+=======
+#include <assert.h>
+using namespace std;
+
+#define BUF_SIZE 1024
+
+void readFile(char* filename, char str[1024])
+{
+	int i=0;
+
+	ifstream fin (filename, ios_base::in | ios_base::binary);
+	
+	if (fin.is_open())
+	{   
+		fin.read(str, 1024);
+	}   
+	else
+	{   
+		return;
+	}   
+	
+	fin.close();
+>>>>>>> fd78047... file I/O
 }
 
-
-void writeFile(char filename[])
+void writeFile(char* filename, char* str, int size)
 {
+<<<<<<< HEAD
 	ofstream w;
 	w.open (filename);
 
 	w.close();
+=======
+	ofstream fout (filename, ios_base::out | ios_base::trunc | ios_base::binary);
+
+	if(fout.is_open())
+	{
+		fout.write(str, size);
+	}   
+	else
+	{   
+		return;
+	}  
+	
+	fout.close();
+>>>>>>> fd78047... file I/O
 }
